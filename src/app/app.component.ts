b7f2ca2d638b1fa9aacd1c3783e1735b8f027db5
Biswadeep Ray <biswa7102raydeep@gmail.com>
2023-10-27 14:24:53 +0530

    init method added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-admin';
+
+  constructor(){
+    if(window.localStorage.getItem("userData") === null){
+      let data = [
+        {
+          "username" : "abc",
+          "password" : "123"
+        }
+      ];
+
+      window.localStorage.setItem("userData",JSON.stringify(data));
+    }
+
+    if (window.localStorage.getItem("productData") === null) {
+      let data = [
+        {
+          "id": "1",
+          "name": "Chocolate",
+          "price": 90
+        },
+        {
+          "id": "2",
+          "name": "Bag",
+          "price": 125
+        },
+        {
+          "id": "3",
+          "name": "Headphones",
+          "price": 350
+        },
+      ];
+
+      window.localStorage.setItem("productData", JSON.stringify(data));
+    }
+
+
+  }
 }
